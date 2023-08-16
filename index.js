2c9ccd5866d97646af4845f4cca7199aad631e65
ASobirjonov074 <jonic607@gmail.com>
2023-08-16 18:13:29 +0500

    first commit

--- index.js
+++ index.js
@@ -34,5 +34,20 @@ silver.onclick = () => {
 black.onclick = () => {
     img.setAttribute('src', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-1inch-spaceblack?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1663703840510')
 }
+let model = document.querySelector('.modal_window')
+let model_bg = document.querySelector('.modal_bg')
+let open = document.querySelector('.open')
+let close = document.querySelector('.close')
+open.onclick = () => {
+    model.style.display = "block"
+    model_bg.style.display = "block"
+}
+close.onclick = () => {
+    model.style.display = "none"
+    model_bg.style.display = "none"
+}
 
-
+model_bg.onclick = () => {
+    model.style.display = "none"
+    model_bg.style.display = "none"
+}
--- index2.js
+++ index2.js
@@ -2,8 +2,8 @@ let btns = document.querySelectorAll('.button_right_left')
 let img = document.querySelector('.picture')
 
 const images = {
-    white: "img/Macbook.png",
-        silver: "img/mbp14-silver-select-202110 1.png"
+    "white": "img/Macbook.png",
+    "silver": "img/mbp14-silver-select-202110 1.png"
 }
 
 btns.forEach(btn => {
@@ -12,8 +12,23 @@ btns.forEach(btn => {
         btn.classList.add('active')
         // const image = images[btn.getAttribute('data-col')]
         let key = btn.getAttribute('data-col')
+
         img.src = images[key]
     }
 
 //     img.setAttribute('src', img)
+})
+
+let buttons = document.querySelectorAll('button'[data-add])
+let newPrice = document.querySelector('.storage')
+
+buttons.forEach(btn => {
+    btn.onclick = () => {
+        let priceAdd = +btn.getAttribute('data-add')
+
+
+        let total = +storage + priceAdd
+
+        buttons.value = total
+    }
 })
